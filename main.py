456b7b4289cc961765456c155b703baa20cc67ac
alanlfreitas <113162737+alanlfreitas@users.noreply.github.com>
2023-09-25 16:27:24 +0000

    word list and ASCI art modules added

--- main.py
+++ main.py
@@ -1,9 +1,69 @@
 # -------------------- HangMan Project -------------------- #
-#Step 1 
 import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+
 word_list = ["aardvark", "baboon", "camel", "gorilla", "kangaroo", "duck", "spider"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
+
+#Creating variable to set the amount of lives
+lives = 6
 
 #Testing the code
 print(f'The random word is: {chosen_word}.')
@@ -18,14 +78,19 @@ end_game = False
 while not end_game:
   guess = input("Guess a letter: ").lower()
   
-  #Check guessed letter
+#Check guessed letter
   for position in range(word_length):
     letter = chosen_word[position]
-    #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
     if letter == guess:
-        display[position] = letter
-  
+      display[position] = letter
+  if guess not in chosen_word:
+    lives -= 1
+    if lives == 0:
+      end_game = True
+      print("You lose.")
   print(display)
+  print(stages[lives])
   if "_" not in display:
     end_game = True
     print("You win.")
+print(stages[lives])
